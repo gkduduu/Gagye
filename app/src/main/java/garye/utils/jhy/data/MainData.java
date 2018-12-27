a2e223cf8dfc045f517924793e1edcacc19af508
hayoung <gkduduu@naver.com>
2018-12-27 17:19:26 +0900

    cm

---
+++ app/src/main/java/garye/utils/jhy/data/MainData.java
@@ -0,0 +1,12 @@
+package garye.utils.jhy.data;
+
+import java.io.Serializable;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by hayoung on 27/12/2018.
+ * gkduduu@naver.com
+ */
+public class MainData extends RealmObject implements Serializable {
+}
